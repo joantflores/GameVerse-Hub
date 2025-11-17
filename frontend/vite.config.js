a222c14b2d7d81977f180110459e5e2d8ccc079a
José Flores <jose.flores@cetys.edu.mx>
2025-11-17 12:50:22 -0800

    Update changes frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-            "/api": "http://localhost:3000"
-        }
-    }
+  plugins: [react()],
+  server: { port: 5173 },
+  preview: { port: 4173 },
+  build: { outDir: "dist" }
 });
