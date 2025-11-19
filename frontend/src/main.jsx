994799f2cf586cd83c5bf1d5b7f57eaf7ba963ad
José Flores <jose.flores@cetys.edu.mx>
2025-11-19 09:46:13 -0800

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,5 @@
 ﻿import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
