61809f0a23e49256fbad44a130cfa907d7d71907
José Flores <jose.flores@cetys.edu.mx>
2025-11-19 23:07:50 -0800

    Delete New Trash Files

--- backend/routes/triviaRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getTriviaQuestions } from "../controllers/triviaController.js";
-
-const router = express.Router();
-
-router.get("/questions", getTriviaQuestions);
-
-export default router;
